9713f1bbef066a9322f72091799141efb0a5505e
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-16 07:52:59 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = 5;
+if(number%2 ==0)
+{
+    Console.Write("number = ");
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.Write("number = ");
+    Console.WriteLine("нечетное");
+}
