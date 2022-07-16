0c2be136549206c85c65fecb312efd0d5ef91add
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-16 17:19:46 +0300

    Короткое решение

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,13 +1,7 @@
 ﻿int a = 5;
 int b = 7;
 int max = a;
-if(a > max) 
-{
-    max = a;
-}
-else
-{
-    max = b;
-}
+if(a > max) max = a;
+else max = b;
 Console.Write("max = ");
 Console.WriteLine (max);   
