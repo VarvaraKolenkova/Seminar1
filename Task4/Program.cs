769174e6513e50afc7aad96f315ec6e61336922d
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-16 08:35:08 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = 5;
+int i = 1;
+while(i <= number)
+{
+    if(i%2 == 0)
+    {
+        Console.WriteLine(i);
+        i+=1;
+    }
+    else
+    {
+        i+=1;
+    }
+}
+Console.WriteLine("The end");
