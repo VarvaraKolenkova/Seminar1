d633ab5ad0d5965dff8ea880971b833db96b3f33
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-16 17:27:36 +0300

    Короткая версия e enter the commit message for your changes. Lines starting

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,17 +2,8 @@
 int b = 3;
 int c = 7;
 int max = a;
-if(a > max)
-{
-    max = a;
-}
-if(b > max)
-{
-    max = b;
-}
-if(c > max)
-{
-    max = c;
-}
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
 Console.Write("max = ");
 Console.WriteLine(max);
