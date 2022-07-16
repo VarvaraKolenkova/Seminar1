73662d4b6965f0a25693064837002e04da7344f0
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-16 18:02:45 +0300

    Короткое решение

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,15 +1,9 @@
-﻿int number = 5;
+﻿int number = 10;
 int i = 1;
 while(i <= number)
 {
-    if(i%2 == 0)
-    {
-        Console.WriteLine(i);
-        i+=1;
-    }
-    else
-    {
-        i+=1;
-    }
+    if(i%2 == 0) 
+    Console.WriteLine(i);
+    i+=1;
 }
 Console.WriteLine("The end");
